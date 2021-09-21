08cd92c2029cf538bf16f79390361003c9ac84a7
Viktor Siedov <viktorrabota22@gmail.com>
2021-09-21 16:37:48 +0300

    commit changes in test

--- SingleAppGit/SingleAppGitTests/SingleAppGitTests.swift
+++ SingleAppGit/SingleAppGitTests/SingleAppGitTests.swift
@@ -18,16 +18,7 @@ class SingleAppGitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    func testUnitExample1() throws {}
+    func testUnitExample2() throws {}
+    func testUnitExample3() throws {}
 }
